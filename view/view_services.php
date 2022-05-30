a30ab1f541c91be1d515fc2f7bbe07990cf32a1e
alekzihz <57765568+alekzihz@users.noreply.github.com>
2022-05-30 18:38:01 +0200

    links_contenido_services
    texto, y redireccionamiento en el apartado de servicios.

--- view/view_services.php
+++ view/view_services.php
@@ -1,15 +1,15 @@
 <img src='img/Listado.jpg' alt="">
     <div class="Listado-servicios">
         <div class="service">
-            <h3 class="n-service"><span class="number">1</span>Terapias</h3>
+            <h3 class="n-service"><span class="number">1</span><a href="index.php?action=servicios&id=1">Terapias</h3>
             <p>Ofecemos consultas online y presenciales para adultos, adolescentes y niños. Intentamos adaptarnos a cada persona y sus peculiaridades</p>
         </div>
         <div class="service">
-            <h3 class="n-service"><span class="number">2</span>Coaching</h3>
+            <h3 class="n-service"><span class="number">2</span><a href="index.php?action=servicios&id=2">Coaching</h3>
             <p>Con metodos motivacionales realistas, intentamos sacar lo mejor de las personas para que crean más en sí mismos y sus capacidades</p>
         </div>
         <div class="service">
-            <h3 class="n-service"><span class="number">3</span>Ayuda Especializada</h3>
+            <h3 class="n-service"><span class="number">3</span><a href="index.php?action=servicios&id=3">Ayuda Especializada</h3>
             <p>ESto ya es fliparse colega jajajajajaja. Mery se ha venido arriba ;)</p>
         </div>
     </div>
